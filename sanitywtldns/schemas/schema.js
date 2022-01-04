c8820ba8d15bcecbb5f8ed28307a960fd8d79999
CarlosRPaz <trojan.carlos44@gmail.com>
2022-01-04 13:59:23 -0800

    safe commit

--- sanitywtldns/schemas/schema.js
+++ sanitywtldns/schemas/schema.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // First, we must import the schema creator
 import createSchema from "part:@sanity/base/schema-creator";
 
@@ -32,34 +31,3 @@ export default createSchema({
     blockContent
   ])
 });
-=======
-// First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from "all:part:@sanity/base/schema-type";
-
-// We import object and document schemas
-import blockContent from "./blockContent";
-import post from "./post";
-import author from "./author";
-import fanImage from "./fanImage";
-
-// Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: "default",
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    post,
-    author,
-    fanImage,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent
-  ])
-});
->>>>>>> ef19a1fc17eab43468e5e22e88068f7e04f62b89
